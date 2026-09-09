94bb0d050526d2d974aecc2009354cf611cf968b
Carlos Sergio de Oliveira <carlossergio_20@hotmail.com>
2026-09-09 13:20:27 -0300

    Criando modulos configurando e ajustando summernote

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,9 @@
+from typing import Any
+
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from django.http import HttpRequest
+from blog.models import Tag, Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -24,7 +28,8 @@ class CategoryAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
     search_fields = 'id', 'slug','title', 'content',
@@ -35,3 +40,27 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields ={
         "slug":('title',),
     }
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = 'id', 'title', 'is_published', 'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug','title', 'content', 'excerpt',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
+    prepopulated_fields ={
+        "slug":('title',),
+    }
+    autocomplete_fields = 'tag', 'category',
+
+    def save_model(self, request,obj, form, change ):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+
+        obj.save()
