378085b0b60a625bd92dc7882607d9206f69bc64
Carlos Sergio de Oliveira <carlossergio_20@hotmail.com>
2026-09-10 13:10:42 -0300

    Cofigirando link para acessar o post na area admin

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.http import HttpRequest
 from blog.models import Tag, Category, Page, Post
 from django_summernote.admin import SummernoteModelAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -51,12 +53,20 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by',\
+          'created_by', 'link',
     prepopulated_fields ={
         "slug":('title',),
     }
     autocomplete_fields = 'tag', 'category',
 
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+        url_do_post = obj.absolute_url()
+        safe_link = mark_safe(f'<a target="_blank" href="{url_do_post}">Ver Post</a>')
+        return safe_link
+    
     def save_model(self, request,obj, form, change ):
         if change:
             obj.updated_by = request.user
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -3,6 +3,7 @@ from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.imagens import resize_image
 from django_summernote.models import AbstractAttachment
+from django.urls import reverse
 
 class PostAttachment(AbstractAttachment):
     def save(self, *args, **kwargs):
@@ -82,10 +83,19 @@ class Page(models.Model):
     def __str__(self):
             return self.title
 
+class PostManager(models.Manager):
+     def get_published(self):
+        return self\
+            .filter(is_published= True)\
+            .order_by('-pk')
+
+          
 class Post(models.Model):
     class Meta:
             verbose_name = 'Post'
             verbose_name_plural = 'Posts'
+
+    objects = PostManager()
 
     title = models.CharField(max_length=65)
     slug = models.SlugField(
@@ -131,6 +141,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:post', args=(self.slug,))
+             
+    
     
     def save(self, *args, **Kwargs):
         if not self.slug:
